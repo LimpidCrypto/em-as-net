a6883d8d4674be68c5a3bd41efb783140fbce4e7
LimpidCrypto <info@limpidcrypto.de>
2023-04-20 22:57:01 +0200

    add framed

---
+++ src/core/framed/framed_impl.rs
@@ -0,0 +1,308 @@
+//! A no_std implementation of https://github.com/tokio-rs/tokio/blob/master/tokio-util/src/codec/framed_impl.rs
+
+use bytes::{Buf, BufMut, BytesMut};
+use core::borrow::{Borrow, BorrowMut};
+use core::mem::MaybeUninit;
+use core::pin::Pin;
+use core::task::{Context, Poll};
+use futures::{ready, Sink, Stream};
+use pin_project_lite::pin_project;
+use super::super::io::{AsyncWrite, AsyncRead, io_slice::IoSlice};
+use super::codec::{Encoder, Decoder};
+use super::exceptions::FramedException;
+
+#[cfg(feature = "std")]
+use tokio::io::ReadBuf;
+#[cfg(not(feature = "std"))]
+use crate::core::io::ReadBuf;
+
+const INITIAL_CAPACITY: usize = 8 * 1024;
+
+pin_project! {
+    #[derive(Debug)]
+    pub(crate) struct FramedImpl<T, C, State>
+    {
+        #[pin]
+        pub(crate) inner: T,
+        pub(crate) state: State,
+        pub(crate) codec: C,
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct ReadFrame {
+    pub(crate) eof: bool,
+    pub(crate) is_readable: bool,
+    pub(crate) buffer: BytesMut,
+    pub(crate) has_errored: bool,
+}
+
+pub(crate) struct WriteFrame {
+    pub(crate) buffer: BytesMut,
+    pub(crate) backpressure_boundary: usize,
+}
+
+#[derive(Default)]
+pub(crate) struct RWFrames {
+    pub(crate) read: ReadFrame,
+    pub(crate) write: WriteFrame,
+}
+
+impl Default for ReadFrame {
+    fn default() -> Self {
+        Self {
+            eof: false,
+            is_readable: false,
+            buffer: BytesMut::with_capacity(INITIAL_CAPACITY),
+            has_errored: false,
+        }
+    }
+}
+
+impl Default for WriteFrame {
+    fn default() -> Self {
+        Self {
+            buffer: BytesMut::with_capacity(INITIAL_CAPACITY),
+            backpressure_boundary: INITIAL_CAPACITY,
+        }
+    }
+}
+
+impl From<BytesMut> for ReadFrame {
+    fn from(mut buffer: BytesMut) -> Self {
+        let size = buffer.capacity();
+        if size < INITIAL_CAPACITY {
+            buffer.reserve(INITIAL_CAPACITY - size);
+        }
+
+        Self {
+            buffer,
+            is_readable: size > 0,
+            eof: false,
+            has_errored: false,
+        }
+    }
+}
+
+impl From<BytesMut> for WriteFrame {
+    fn from(mut buffer: BytesMut) -> Self {
+        let size = buffer.capacity();
+        if size < INITIAL_CAPACITY {
+            buffer.reserve(INITIAL_CAPACITY - size);
+        }
+
+        Self { buffer, backpressure_boundary: INITIAL_CAPACITY, }
+    }
+}
+
+impl Borrow<ReadFrame> for RWFrames {
+    fn borrow(&self) -> &ReadFrame {
+        &self.read
+    }
+}
+impl BorrowMut<ReadFrame> for RWFrames {
+    fn borrow_mut(&mut self) -> &mut ReadFrame {
+        &mut self.read
+    }
+}
+impl Borrow<WriteFrame> for RWFrames {
+    fn borrow(&self) -> &WriteFrame {
+        &self.write
+    }
+}
+impl BorrowMut<WriteFrame> for RWFrames {
+    fn borrow_mut(&mut self) -> &mut WriteFrame {
+        &mut self.write
+    }
+}
+
+
+/*
+TODO: Wake `cx`
+ */
+
+
+impl<T, U, R> Stream for FramedImpl<T, U, R>
+    where
+        T: AsyncRead,
+        U: Decoder,
+        R: BorrowMut<ReadFrame>,
+{
+    type Item = Result<U::Item, U::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut pinned = self.project();
+        let state: &mut ReadFrame = pinned.state.borrow_mut();
+
+        loop {
+            if state.has_errored {
+                state.is_readable = false;
+                state.has_errored = false;
+                return Poll::Ready(None);
+            }
+
+            if state.is_readable {
+                if state.eof {
+                    let frame = pinned.codec.decode_eof(&mut state.buffer).map_err(|err| {
+                        state.has_errored = true;
+                        err
+                    })?;
+                    if frame.is_none() {
+                        state.is_readable = false;
+                    }
+                    return Poll::Ready(frame.map(Ok));
+                }
+
+                if let Some(frame) = pinned.codec.decode(&mut state.buffer).map_err(|op| {
+                    state.has_errored = true;
+                    op
+                })? {
+                    return Poll::Ready(Some(Ok(frame)));
+                }
+
+                state.is_readable = false;
+            }
+
+            state.buffer.reserve(1);
+            let bytect = match poll_read_buf(pinned.inner.as_mut(), cx, &mut state.buffer).map_err(
+                |_| {FramedException::UnableToRead}
+            )? {
+                Poll::Ready(ct) => ct,
+                Poll::Pending => return Poll::Pending,
+            };
+            if bytect == 0 {
+                if state.eof {
+                    return Poll::Ready(None);
+                }
+                state.eof = true;
+            } else {
+                state.eof = false;
+            }
+
+            state.is_readable = true;
+        }
+    }
+}
+
+impl<T, I, U, W> Sink<I> for FramedImpl<T, U, W>
+    where
+        T: AsyncWrite,
+        U: Encoder<I>,
+        W: BorrowMut<WriteFrame>,
+{
+    type Error = FramedException;
+
+    fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        if self.state.borrow().buffer.len() >= self.state.borrow().backpressure_boundary {
+            self.as_mut().poll_flush(cx)
+        } else {
+            Poll::Ready(Ok(()))
+        }
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: I) -> Result<(), Self::Error> {
+        let pinned = self.project();
+        match pinned
+            .codec
+            .encode(item, &mut pinned.state.borrow_mut().buffer) {
+            Ok(_) => { Ok(()) }
+            Err(_) => { Err(FramedException::UnableToEncode) }
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        let mut pinned = self.project();
+
+        while !pinned.state.borrow_mut().buffer.is_empty() {
+            let WriteFrame { buffer, .. } = pinned.state.borrow_mut();
+
+            let n = ready!(poll_write_buf(pinned.inner.as_mut(), cx, buffer))?;
+
+            if n == 0 {
+                return Poll::Ready(Err(FramedException::UnableToFlush));
+            }
+        }
+
+        ready!(pinned.inner.poll_flush(cx))?;
+
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        ready!(self.as_mut().poll_flush(cx))?;
+        ready!(self.project().inner.poll_shutdown(cx))?;
+
+        Poll::Ready(Ok(()))
+    }
+}
+
+pub fn poll_read_buf<T: AsyncRead, B: BufMut>(
+    io: Pin<&mut T>,
+    cx: &mut Context<'_>,
+    buf: &mut B,
+) -> Poll<Result<usize, T::Error>> {
+    if !buf.has_remaining_mut() {
+        return Poll::Ready(Ok(0));
+    }
+
+    let n = {
+        let dst = buf.chunk_mut();
+
+        // Safety: `chunk_mut()` returns a `&mut UninitSlice`, and `UninitSlice` is a
+        // transparent wrapper around `[MaybeUninit<u8>]`.
+        let dst = unsafe { &mut *(dst as *mut _ as *mut [MaybeUninit<u8>]) };
+        let mut buf = ReadBuf::uninit(dst);
+        let ptr = buf.filled().as_ptr();
+        ready!(io.poll_read(cx, &mut buf)?);
+
+        // Ensure the pointer does not change from under us
+        assert_eq!(ptr, buf.filled().as_ptr());
+        buf.filled().len()
+    };
+
+    // Safety: This is guaranteed to be the number of initialized (and read)
+    // bytes due to the invariants provided by `ReadBuf::filled`.
+    unsafe {
+        buf.advance_mut(n);
+    }
+
+    Poll::Ready(Ok(n))
+}
+
+pub fn poll_write_buf<T: AsyncWrite, B: Buf>(
+    io: Pin<&mut T>,
+    cx: &mut Context<'_>,
+    buf: &mut B,
+) -> Poll<Result<usize, FramedException>> {
+    const MAX_BUFS: usize = 64;
+
+    if !buf.has_remaining() {
+        return Poll::Ready(Ok(0));
+    }
+
+    let n = if io.is_write_vectored() {
+        let mut slices = [IoSlice::new(&[]); MAX_BUFS];
+        let cnt = chunks_vectored(&buf, &mut slices);
+        ready!(io.poll_write_vectored(cx, &slices[..cnt]))?
+    } else {
+        ready!(io.poll_write(cx, buf.chunk()))?
+    };
+
+    buf.advance(n);
+
+    Poll::Ready(Ok(n))
+}
+
+fn chunks_vectored<'a, B: Buf>(buf: &'a B, dst: &mut [IoSlice<'a>]) -> usize
+{
+    if dst.is_empty() {
+        return 0;
+    }
+
+    if buf.has_remaining() {
+        dst[0] = IoSlice::new(buf.chunk());
+        1
+    } else {
+        0
+    }
+}
---
+++ src/core/framed/mod.rs
@@ -0,0 +1,168 @@
+//! A no_std version of `tokio::Framed`
+
+pub mod codec;
+pub use codec::Codec;
+
+mod framed_impl;
+use framed_impl::{FramedImpl, RWFrames, ReadFrame, WriteFrame};
+
+pub mod exceptions;
+pub use exceptions::*;
+
+use core::fmt;
+use core::pin::Pin;
+use core::task::{Context, Poll};
+use bytes::BytesMut;
+use futures::{Sink, Stream};
+use pin_project_lite::pin_project;
+
+use super::io::{AsyncRead, AsyncWrite};
+
+pub use exceptions::FramedException;
+
+use codec::{Encoder, Decoder};
+
+pin_project! {
+    pub struct Framed<T, U> {
+        #[pin]
+        pub(crate) inner: FramedImpl<T, U, RWFrames>
+    }
+}
+
+impl<T, U> Framed<T, U>
+    where
+        T: AsyncRead + AsyncWrite,
+{
+    pub fn new(inner: T, codec: U) -> Framed<T, U> {
+        Framed {
+            inner: FramedImpl {
+                inner,
+                codec,
+                state: Default::default(),
+            },
+        }
+    }
+
+    pub fn with_capacity(inner: T, codec: U, capacity: usize) -> Framed<T, U> {
+        Framed {
+            inner: FramedImpl {
+                inner,
+                codec,
+                state: RWFrames {
+                    read: ReadFrame {
+                        eof: false,
+                        is_readable: false,
+                        buffer: BytesMut::with_capacity(capacity),
+                        has_errored: false,
+                    },
+                    write: WriteFrame::default(),
+                },
+            },
+        }
+    }
+}
+
+impl<T, U> Framed<T, U> {
+    pub fn get_ref(&self) -> &T {
+        &self.inner.inner
+    }
+
+    pub fn get_mut(&mut self) -> &mut T {
+        &mut self.inner.inner
+    }
+
+    pub fn get_pin_mut(self: Pin<&mut Self>) -> Pin<&mut T> {
+        self.project().inner.project().inner
+    }
+
+    pub fn codec(&self) -> &U {
+        &self.inner.codec
+    }
+
+    pub fn codec_mut(&mut self) -> &mut U {
+        &mut self.inner.codec
+    }
+
+    pub fn codec_pin_mut(self: Pin<&mut Self>) -> &mut U {
+        self.project().inner.project().codec
+    }
+
+    pub fn read_buffer(&self) -> &BytesMut {
+        &self.inner.state.read.buffer
+    }
+
+    pub fn read_buffer_mut(&mut self) -> &mut BytesMut {
+        &mut self.inner.state.read.buffer
+    }
+
+    pub fn write_buffer(&self) -> &BytesMut {
+        &self.inner.state.write.buffer
+    }
+
+    pub fn write_buffer_mut(&mut self) -> &mut BytesMut {
+        &mut self.inner.state.write.buffer
+    }
+
+    pub fn backpressure_boundary(&self) -> usize {
+        self.inner.state.write.backpressure_boundary
+    }
+
+    pub fn set_backpressure_boundary(&mut self, boundary: usize) {
+        self.inner.state.write.backpressure_boundary = boundary;
+    }
+
+    pub fn into_inner(self) -> T {
+        self.inner.inner
+    }
+}
+
+// This impl just defers to the underlying FramedImpl
+impl<T, U> Stream for Framed<T, U>
+    where
+        T: AsyncRead,
+        U: Decoder,
+{
+    type Item = Result<U::Item, U::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.project().inner.poll_next(cx)
+    }
+}
+
+// This impl just defers to the underlying FramedImpl
+impl<T, I, U> Sink<I> for Framed<T, U>
+    where
+        T: AsyncWrite,
+        U: Encoder<I>,
+{
+    type Error = FramedException;
+
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_ready(cx)
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: I) -> Result<(), Self::Error> {
+        self.project().inner.start_send(item)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_close(cx)
+    }
+}
+
+impl<T, U> fmt::Debug for Framed<T, U>
+    where
+        T: fmt::Debug,
+        U: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Framed")
+            .field("io", self.get_ref())
+            .field("codec", self.codec())
+            .finish()
+    }
+}
