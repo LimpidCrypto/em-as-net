7010918db31761738f3c02505e74c35431b66be5
LimpidCrypto <info@limpidcrypto.de>
2023-05-06 16:24:26 +0200

    add example tls.rs (not working yet)

--- examples/std-tokio/src/bin/tls.rs
+++ examples/std-tokio/src/bin/tls.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use embedded_tls::Aes128GcmSha256;
+
+use tokio::net;
+use em_as_net::core::tcp::{TcpStream};
+use em_as_net::core::tls::{FromTokio, TlsConnection};
+
+#[tokio::main]
+async fn main() {
+    let mut read_buffer = [0u8; 16384];
+    let mut write_buffer = [0u8; 16384];
+
+    let stream: TcpStream<net::TcpStream> = TcpStream::new();
+    let adapter = FromTokio::new(stream);
+    let tls: TlsConnection<FromTokio<TcpStream<net::TcpStream>>, Aes128GcmSha256> = TlsConnection::new();
+    tls.open("limpidcrypto.de:6004".into(), adapter, &mut read_buffer, &mut write_buffer).await.unwrap();
+    // adapter.connect().await.unwrap();
+    //
+    //
+    // // Write to stream
+    // adapter.write(b"Hello!").await.unwrap();
+    // println!("written");
+    // adapter.flush().await.unwrap();
+    // println!("flushed");
+    //
+    // while let Ok(s) = adapter.read(&mut buffer).await {
+    //     println!("{:?}", s);
+    //     adapter.shutdown().await.unwrap();
+    //     println!("shutdown");
+    //     break;
+    // }
 }
