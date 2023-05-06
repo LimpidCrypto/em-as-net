19846ed09a4f3e0f5dba11a6a7b0faeea0a15d0b
LimpidCrypto <info@limpidcrypto.de>
2023-05-06 16:29:00 +0200

    update WebsocketClient

--- src/client/websocket/mod.rs
+++ src/client/websocket/mod.rs
@@ -1,34 +1,40 @@
 pub mod errors;
 
-pub use embedded_websocket::{WebSocketOptions, framer_async::ReadResult};
+use crate::core::framed::{Codec, Framed};
+use crate::core::tcp::TcpStream;
+use alloc::borrow::Cow;
+use anyhow::Result;
+use core::cell::RefCell;
+use embedded_websocket::framer_async::Framer;
+pub use embedded_websocket::{framer_async::ReadResult, WebSocketOptions};
+use embedded_websocket::{Client, WebSocketSendMessageType};
+use rand_core::RngCore;
 
-#[cfg(feature = "std")]
-pub use if_std::WebsocketClient;
+pub struct WebsocketClient<'a, T, U: RngCore> {
+    uri: Cow<'a, str>,
+    buffer: &'a mut [u8],
+    stream: RefCell<Option<Framed<TcpStream<T>, Codec>>>,
+    framer: RefCell<Option<Framer<U, Client>>>,
+}
 
 #[cfg(feature = "std")]
 mod if_std {
-    use anyhow::Result;
-    use alloc::borrow::{Cow, ToOwned};
-    use core::cell::RefCell;
-    use embedded_websocket::{Client, WebSocketCloseStatusCode, WebSocketSendMessageType};
-    use embedded_websocket::framer_async::{Framer, ReadResult};
-    use tokio::net;
     use crate::core::framed::{Codec, Framed};
     use crate::core::tcp::{Connect, TcpStream};
+    use alloc::borrow::{Cow, ToOwned};
+    use anyhow::Result;
+    use core::cell::RefCell;
+    use embedded_websocket::framer_async::{Framer, ReadResult};
+    use embedded_websocket::{WebSocketCloseStatusCode, WebSocketSendMessageType};
+    use tokio::net;
 
+    use crate::client::websocket::errors::WebsocketError;
+    use crate::client::websocket::{WebsocketClient, WebsocketClientIo};
+    use crate::Err;
     use embedded_websocket::WebSocketOptions;
     use rand::rngs::ThreadRng;
-    use crate::client::websocket::errors::WebsocketError;
-    use crate::Err;
 
-    pub struct WebsocketClient<'a> {
-        uri: Cow<'a, str>,
-        buffer: &'a mut [u8],
-        stream: RefCell<Option<Framed<TcpStream<net::TcpStream>, Codec>>>,
-        framer: RefCell<Option<Framer<ThreadRng, Client>>>,
-    }
-
-    impl<'a> WebsocketClient<'a> {
+    impl<'a> WebsocketClient<'a, net::TcpStream, ThreadRng> {
         pub fn new(uri: Cow<'a, str>, buffer: &'a mut [u8]) -> Self {
             Self {
                 uri,
@@ -49,43 +55,90 @@ mod if_std {
             let ws_client = embedded_websocket::WebSocketClient::new_client(rng);
             // Connect Websocket
             let mut framer = Framer::new(ws_client);
-            framer.connect(&mut self.stream.borrow_mut().as_mut().unwrap(), self.buffer, &options).await.unwrap();
+            framer
+                .connect(
+                    &mut self.stream.borrow_mut().as_mut().unwrap(),
+                    self.buffer,
+                    &options,
+                )
+                .await
+                .unwrap();
 
             self.framer.replace(Some(framer));
         }
     }
 
-    impl<'a> WebsocketClient<'a> {
-        pub async fn read(&mut self) -> Option<Result<ReadResult<'_>>> {
-            let read_result = self.framer.borrow_mut().as_mut().unwrap().read( // TODO: handle unwrap
-                self.stream.borrow_mut().as_mut().unwrap(),
-                self.buffer
-            ).await;
+    impl<'a> WebsocketClientIo<'a> for WebsocketClient<'a, net::TcpStream, ThreadRng> {
+        async fn read(&mut self) -> Option<Result<ReadResult<'_>>> {
+            let read_result = self
+                .framer
+                .borrow_mut()
+                .as_mut()
+                .unwrap()
+                .read(
+                    // TODO: handle unwrap
+                    self.stream.borrow_mut().as_mut().unwrap(),
+                    self.buffer,
+                )
+                .await;
 
             match read_result {
-                Some(Err(_)) => {
-                    Some(Err!(WebsocketError::ReadError))
-                },
+                Some(Err(err)) => Some(Err!(WebsocketError::<anyhow::Error>::from(err))),
                 Some(Ok(read_res)) => Some(Ok(read_res)),
-                None => None
+                None => None,
             }
         }
-        pub async fn write(&mut self, message: Cow<'a, str> ) {
-            self.framer.borrow_mut().as_mut().unwrap().write( // TODO: handle unwrap
-                self.stream.borrow_mut().as_mut().unwrap(),
-                self.buffer,
-                WebSocketSendMessageType::Text,
-                true,
-                message.as_ref().as_bytes()
-            ).await.unwrap() // TODO: Return `Result`
+
+        async fn write(
+            &mut self,
+            message: Cow<'a, str>,
+            send_msg_type: Option<WebSocketSendMessageType>,
+        ) -> Result<()> {
+            return match self.framer
+                .borrow_mut()
+                .as_mut()
+                .unwrap()
+                .write(
+                    // TODO: handle unwrap0
+                    match self.stream.borrow_mut().as_mut() {
+                        None => { return Err!(WebsocketError::<anyhow::Error>::NotConnected); },
+                        Some(stream) => stream
+                    },
+                    self.buffer,
+                    send_msg_type.unwrap_or(WebSocketSendMessageType::Text),
+                    true,
+                    message.as_ref().as_bytes(),
+                )
+                .await {
+                Ok(()) => Ok(()),
+                Err(err) => Err!(WebsocketError::from(err))
+            }
         }
-        pub async fn close(&mut self) {
-            self.framer.borrow_mut().as_mut().unwrap().close( // TODO: handle unwrap
-                self.stream.borrow_mut().as_mut().unwrap(),
-                self.buffer,
-                WebSocketCloseStatusCode::NormalClosure,
-                None,
-            ).await.unwrap() // TODO: Return `Result`
+
+        async fn close(&mut self) {
+            self.framer
+                .borrow_mut()
+                .as_mut()
+                .unwrap()
+                .close(
+                    // TODO: handle unwrap
+                    self.stream.borrow_mut().as_mut().unwrap(),
+                    self.buffer,
+                    WebSocketCloseStatusCode::NormalClosure,
+                    None,
+                )
+                .await
+                .unwrap() // TODO: Return `Result`
         }
     }
+}
+
+pub trait WebsocketClientIo<'a> {
+    async fn read(&mut self) -> Option<Result<ReadResult<'_>>>;
+    async fn write(
+        &mut self,
+        message: Cow<'a, str>,
+        send_msg_type: Option<WebSocketSendMessageType>,
+    ) -> Result<()>;
+    async fn close(&mut self);
 }
