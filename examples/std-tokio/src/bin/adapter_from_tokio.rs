fa9521c2844d75db7ff42871f6a4b8ec8dfbfe30
LimpidCrypto <info@limpidcrypto.de>
2023-05-06 16:23:57 +0200

    add example adapter_from_tokio.rs (not working yet)

--- examples/std-tokio/src/bin/adapter_from_tokio.rs
+++ examples/std-tokio/src/bin/adapter_from_tokio.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use embedded_io::asynch::{Read, Write};
+
+use tokio::net;
+use em_as_net::core::tcp::{Connect, TcpStream};
+use em_as_net::core::tls::{FromTokio, TlsConnection};
+
+#[tokio::main]
+async fn main() {
+    // Connect to an echo server.
+    let stream: TcpStream<net::TcpStream> = TcpStream::new();
+    let mut adapter = FromTokio::new(stream);
+    adapter.connect("tcpbin.com:4242".into()).await.unwrap();
+
+
+    // Write to stream
+    adapter.write(b"Hello!").await.unwrap();
+    println!("written");
+    adapter.flush().await.unwrap();
+    println!("flushed");
+
+    let mut buffer = [0u8; 4096];
+    while let Ok(s) = adapter.read(&mut buffer).await {
+        println!("{:?}", s);
+        adapter.shutdown().await.unwrap();
+        println!("shutdown");
+        break;
+    }
+}
