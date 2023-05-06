75bc7f238ef27c48e896fd514df1e2d545a6be50
LimpidCrypto <info@limpidcrypto.de>
2023-05-06 16:27:11 +0200

    add WebsocketError and From<FramerError>

--- src/client/websocket/errors.rs
+++ src/client/websocket/errors.rs
@@ -1,14 +1,46 @@
 use core::fmt::Debug;
+use core::str::Utf8Error;
+use embedded_websocket::framer_async::FramerError;
 use thiserror_no_std::Error;
 
-#[derive(Debug, Clone, PartialEq, Eq, Error)]
-pub enum WebsocketError {
-    #[error("Unable to read.")] // TODO: specify error
-    ReadError,
+#[derive(Debug, PartialEq, Eq, Error)]
+pub enum WebsocketError<E: Debug> {
+    #[error("Stream is not connected.")]
+    NotConnected,
+
+    // FramerError
+    #[error("I/O error: {0:?}")]
+    Io(E),
+    #[error("Frame too large (size: {0:?})")]
+    FrameTooLarge(usize),
+    #[error("Failed to interpret u8 to string (error: {0:?})")]
+    Utf8(Utf8Error),
+    #[error("Invalid HTTP header")]
+    HttpHeader,
+    #[error("Websocket error: {0:?}")]
+    WebSocket(embedded_websocket::Error),
+    #[error("Disconnected")]
+    Disconnected,
+    #[error("Read buffer is too small (size: {0:?})")]
+    RxBufferTooSmall(usize),
+}
+
+impl<E: Debug> From<FramerError<E>> for WebsocketError<E> {
+    fn from(value: FramerError<E>) -> Self {
+        match value {
+            FramerError::Io(e) => {WebsocketError::Io(e)}
+            FramerError::FrameTooLarge(e) => {WebsocketError::FrameTooLarge(e)}
+            FramerError::Utf8(e) => {WebsocketError::Utf8(e)}
+            FramerError::HttpHeader(_) => {WebsocketError::HttpHeader}
+            FramerError::WebSocket(e) => {WebsocketError::WebSocket(e)}
+            FramerError::Disconnected => {WebsocketError::Disconnected}
+            FramerError::RxBufferTooSmall(e) => {WebsocketError::RxBufferTooSmall(e)}
+        }
+    }
 }
 
 #[cfg(feature = "std")]
-impl alloc::error::Error for WebsocketError {}
+impl<E: Debug> alloc::error::Error for WebsocketError<E> {}
 
 #[derive(Debug, Clone, PartialEq, Eq, Error)]
 pub enum AddrsError<'a> {
